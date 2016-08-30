ded76994c49c261791a8a8f9d5cc2e64d9f2c1f1
justaskumelis <justaskumelis@gmail.com>
2016-08-30 22:34:27 +0300

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
